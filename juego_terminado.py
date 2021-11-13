40670e1ea07b04afbd9b2bedabff52c96e0cd4ec
Matota <72810000+mat0ta@users.noreply.github.com>
2021-11-13 23:19:08 +0100

    Update juego_terminado.py

--- juego_terminado.py
+++ juego_terminado.py
@@ -5,9 +5,12 @@ from itertools import permutations
 class juegoterminado():
     def __init__(self):
         self.tabla_puntuaciones = []
+        self.nombre = str
         self.dificultad = 1
         self.intentos_permitidos = 10
         self.intentos_realizados = 0
+    def pedir_nombre(self, nombre):
+        self.nombre = str(nombre)
     def  perdir_dificultad(self, nivel):
         while not nivel.isdigit():
             nivel = input('Introduzca un número válido: ')
@@ -15,6 +18,8 @@ class juegoterminado():
             nivel = input('Introduzca un número válido: ')
         self.dificultad = int(nivel)
         print('Dificultad seleccionada: ' + str(self.dificultad))
+    def definir_parametros(self):
+        self.intentos_permitidos = 100 ** int(self.dificultad)
+
 
 a = juegoterminado()
-a.perdir_dificultad(input('Elige el nivel de dificultad entre 1 y 4: '))
